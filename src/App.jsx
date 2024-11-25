ccd9cd3152af60fbfc7ef8a2d0a652f1287ff4ec
Bishesh Marasini <marasinibishesh@gmail.com>
2024-11-25 20:21:58 +0545

    Setup of AICRUSADE

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from './sections/Navigation';
+
+
+
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto'>
+      <Navigation/>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/sections/Navigation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div className='text-white'>Navigation</div>
+  )
+}
+
+export default Navigation
